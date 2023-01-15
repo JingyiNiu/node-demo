afa114b039f77e85ce47c519a7ba330a8c4e43c8
JingyiNiu <niu.jy1203@gmail.com>
2023-01-16 12:01:28 +1300

    aynsc validator

--- mongodb.js
+++ mongodb.js
@@ -22,8 +22,12 @@ const courseSchema = new mongoose.Schema({
     tags: {
         type: Array,
         validate: {
-            validator: function (v) {
-                return v && v.length > 0;
+            validator: async function (v) {
+                // const res = await v && v.length > 0;
+                const res = new Promise((resolve) =>
+                    setTimeout(() => resolve(v && v.length > 0), 3000)
+                );
+                return res;
             },
             message: "A course should have at least one tag",
         },
